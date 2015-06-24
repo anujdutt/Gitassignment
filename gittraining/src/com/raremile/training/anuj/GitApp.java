7a6a8e23b78b114e030882b018605dfac294e0b0
anujdutt <anuj.dutt@raremile.com>
2015-06-24 16:07:18 +0530

    Fixing conflicts

--- gittraining/src/com/raremile/training/anuj/GitApp.java
+++ gittraining/src/com/raremile/training/anuj/GitApp.java
@@ -3,7 +3,7 @@ package com.raremile.training.anuj;
 public class GitApp {
 	public static void main(String [] args){
 
-		System.out.println("My name is <name>, I know a lot more Git. I know how to merge!");
+		System.out.println("My name is Anuj, I know a lot more Git. I know how to merge!");
 
 
 	}
